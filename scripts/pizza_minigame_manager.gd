59ccf8ad5b7d45c0373b253e0738c68618b57eeb
AnandaGI <agirwin42@tntech.edu>
2025-04-24 00:24:38 -0500

    Update pizza minigame
     - Add pizza board to place pizza in oven and remove instance

--- scripts/pizza_minigame_manager.gd
+++ scripts/pizza_minigame_manager.gd
@@ -2,17 +2,25 @@ extends Node
 
 const PIZZA = preload("res://scenes/pizza.tscn")
 
-var pizza_list = []
+var num_pizzas_left = 3
+var pizza_board: Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_create_pizza()
+	pizza_board = get_node("PizzaBoard")
+	pizza_board.is_occupied.connect(_move_board)
+
+func _create_pizza():
 	var pizza = PIZZA.instantiate()
 	add_child(pizza)
-	pizza_list.append(pizza)
 
+func _move_board():
+
+	pizza_board.get_node("AnimationPlayer").play("Move_in_out")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var curr_pizza = pizza_list[0]
+	pass
 
 	
